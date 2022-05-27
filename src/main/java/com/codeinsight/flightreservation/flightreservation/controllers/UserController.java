9ebb2551cac515b3a1e3bb094552d7eea1386f9a
Veselin Sevo <veselinsevo@gmail.com>
2022-05-27 02:24:03 +0200

    Project completed version 1, security added

--- src/main/java/com/codeinsight/flightreservation/flightreservation/controllers/UserController.java
+++ src/main/java/com/codeinsight/flightreservation/flightreservation/controllers/UserController.java
@@ -1,11 +1,13 @@
 package com.codeinsight.flightreservation.flightreservation.controllers;
 
 import com.codeinsight.flightreservation.flightreservation.entities.User;
+import com.codeinsight.flightreservation.flightreservation.services.SecurityService;
 import com.codeinsight.flightreservation.flightreservation.services.UserService;
 import com.codeinsight.flightreservation.flightreservation.util.EmailUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -15,6 +17,12 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class UserController {
+
+    @Autowired
+    private SecurityService securityService;
+
+    @Autowired
+    private BCryptPasswordEncoder encoder;
 
     private static final Logger LOGGER = LoggerFactory.getLogger(UserController.class);
 
@@ -36,6 +44,7 @@ public class UserController {
     }
     @RequestMapping(value = "register", method = RequestMethod.POST)
     public String registerUser(@ModelAttribute("user") User user){
+        user.setPassword(encoder.encode(user.getPassword()));
         if(user.getSubscribed()==null){             //Pitati Vukasina
             user.setSubscribed("not subscribed");
         }
@@ -48,8 +57,8 @@ public class UserController {
     @RequestMapping(value = "login", method = RequestMethod.POST)
     public String loginUser(@RequestParam("email") String email, @RequestParam("password") String password, ModelMap modelMap){
         LOGGER.info("Inside loginUser(), EMAIL: " + email);
-        User user = userService.findUserByEmail(email);
-        if(user != null && user.getPassword().equals(password)){
+        boolean loginResponse = securityService.login(email, password);
+        if(loginResponse){
             return "flights/findFlights";
         }else{
             modelMap.addAttribute("error", "Invalid Username or Password. Please try again.");
---
+++ src/main/java/com/codeinsight/flightreservation/flightreservation/security/config/WebSecurityConfig.java
@@ -0,0 +1,35 @@
+package com.codeinsight.flightreservation.flightreservation.security.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/show-register", "/", "/index.html", "/register", "/show-login", "/login", "/login/*")
+                .permitAll()
+                .antMatchers("/admin/*")
+                .hasAnyAuthority("ADMIN")
+                .anyRequest().authenticated().and().csrf().disable();
+    }
+}
